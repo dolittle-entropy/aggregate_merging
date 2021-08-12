82cc2ce10a86fd93f9bcbf7feb372df74d4f1b24
Tomas Ekeli <tomas@eke.li>
2021-08-12 11:43:35 +0200

    the customer-aggregate assumes the orders
    it contains a dictionary of orders and starts them off by
    assuming the order-aggregate's responsibilities

--- src/Ordering/Domain/CustomerAggregate.cs
+++ src/Ordering/Domain/CustomerAggregate.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using Dolittle.SDK.Aggregates;
 using Dolittle.SDK.Events;
 using Ordering.Events.Customers;
+using Ordering.Events.Orders;
 
 namespace Ordering.Domain
 {
@@ -11,11 +13,12 @@ namespace Ordering.Domain
         bool _created = false;
         EventSourceId _id;
 
+        IDictionary<Guid, OrderState> _orders = new Dictionary<Guid, OrderState>();
+
         public CustomerAggregate(EventSourceId id) : base(id)
         {
             _id = id;
         }
-
 
         public void Create(string name, string email)
         {
@@ -67,6 +70,93 @@ namespace Ordering.Domain
             );
         }
 
+        public void AssumeResponsibilityForOrder(
+            Guid orderId,
+            IList<Guid> items,
+            bool created,
+            bool placed,
+            bool cancelled,
+            bool abandoned
+        )
+        {
+            if (!_created)
+            {
+                // possibly stop? this is a result of no previous check for existing customer
+                // throw new Exception("customer does not exist - cannot have an order");
+
+                Apply(
+                    new CustomerCreated(
+                        _id,
+                        "automatically created customer",
+                        "unknown@test.com"
+                    )
+                );
+            }
+
+            Apply(
+                new OrderAggregateRetired(
+                    orderId: orderId,
+                    customerId: _id,
+                    items: items,
+                    created: created,
+                    placed: placed,
+                    cancelled: cancelled,
+                    abandoned: abandoned
+                )
+            );
+        }
+
+        public void AbandonOrder(Guid orderId)
+        {
+            Apply(
+                GetOrder(orderId).Abandon()
+            );
+        }
+
+        public void AddItemToOrder(Guid orderId, Guid productId, string productName, decimal price)
+        {
+            Apply(
+                GetOrder(orderId).AddItem(productId, productName, price)
+            );
+        }
+
+        public void CancelOrder(Guid orderId)
+        {
+            Apply(
+                GetOrder(orderId).Cancel()
+            );
+        }
+
+        public void CreateOrder(Guid orderId)
+        {
+            Apply(
+                GetOrder(orderId).Cancel()
+            );
+        }
+
+        public void PlaceOrder(Guid orderId)
+        {
+            Apply(
+                GetOrder(orderId).Place()
+            );
+        }
+
+        public void RemoveFromOrderItem(Guid orderId, Guid productId)
+        {
+            Apply(
+                GetOrder(orderId).RemoveItem(productId)
+            );
+        }
+
+        OrderState GetOrder(Guid orderId)
+        {
+            if (!_orders.ContainsKey(orderId))
+            {
+                throw new Exception("customer does not have that order");
+            }
+            return _orders[orderId];
+        }
+
         void On(CustomerCreated evt)
         {
             Console.WriteLine(
@@ -81,6 +171,41 @@ namespace Ordering.Domain
                 $@"{DateTime.UtcNow} - customer-aggregate {_id} handling customer-removed"
             );
             _created = false;
+        }
+
+        void On(OrderAggregateRetired evt)
+        {
+            GetOrder(evt.OrderId).On(evt);
+        }
+
+         void On(OrderCreated evt)
+        {
+            GetOrder(evt.OrderId).On(evt);
+        }
+
+        void On(ItemAddedToOrder evt)
+        {
+            GetOrder(evt.OrderId).On(evt);
+        }
+
+        void On(ItemRemovedFromOrder evt)
+        {
+            GetOrder(evt.OrderId).On(evt);
+        }
+
+        void On(OrderPlaced evt)
+        {
+            GetOrder(evt.OrderId).On(evt);
+        }
+
+        void On(OrderCancelled evt)
+        {
+            GetOrder(evt.OrderId).On(evt);
+        }
+
+        void On(OrderAbandoned evt)
+        {
+            GetOrder(evt.OrderId).On(evt);
         }
     }
 }
